11068cb3f24f3bb043d3185198b224d681d99ef2
Pedro Ramos <pedroramosg09@gmail.com>
2023-05-10 20:50:34 +0200

    💄 buttons added

--- src/Styleguide/views/Colors.jsx
+++ src/Styleguide/views/Colors.jsx
@@ -1,11 +1,23 @@
 import ColorCard from "../components/ColorCard/ColorCard";
 
 export default function Colors() {
+
+  const greyScale = ['grey0', 'grey20', 'grey40', 'grey60', 'grey80', 'grey100'];
+
   return (
     <>
       <article className="sg-section colors">
         <h2>Colors</h2>
 
+        <h3>Grey Scale</h3>
+        <div className="section__list">
+        {
+          greyScale.map(color => (
+            <ColorCard color={color} />
+          ))
+        }
+        </div>
+        
         <div className="section__list">
           <ColorCard color="neutralGray"/>
           <ColorCard color="primary"/>
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,19 @@
 import './Button.scss';
 
-export default function Button({ text }) {
+export default function Button({ el, text, type, href }) {
+
+  const typeClass = type ? `btn-${type}` : '';
 
   return (
     <>
-      <button>{text}</button>
+      {
+        (el === 'btn' || el === '' || el === undefined  || type === null) &&
+        <button className={`btn ${typeClass}`}>{text}</button>
+      }
+      {
+        el === 'link' &&
+        <a className={`btn ${typeClass}`} href={href}>{text}</a>
+      }
     </>
   )
 }
